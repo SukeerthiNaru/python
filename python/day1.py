45dfc5d9fa16040a377fd512146a5e8d607942f3
SukeerthiNaru <sukeerthinaru@gmail.com>
2025-02-09 15:55:26 +0530

    Add files via upload

---
+++ python/day1.py
@@ -0,0 +1,52 @@
+#calculations on arithrmrtic operators#
+print(111+222)
+print(666-222)
+print(555*222)
+print(666/333)
+print(777//222)
+
+#on variables
+a=20
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+
+#strings
+str="kodni koii."
+print(str)
+#length
+print(len(str))
+#type
+print(type(str))
+
+#index
+print(str[0])
+print(str[10])
+print(str[-1])
+print(str[-11])
+
+#slicing in positive nd neg
+str1="koii koii kodni koii"
+print(str1)
+print(str1[0:3])
+print(str1[5:9])
+print(str1[3:10:-1])
+print(str1[3: :-1])
+print(str1[ : :-1])
+
+#memory allocation
+S="statment gets executed"
+K="print false statement"
+print(id(9))
+print(id(K))
+
+#strings cannt change particular variable
+s='su'
+s[1]='v'
+
+#reassign
+s='vw'
+print(s)
